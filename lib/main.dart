85ce7ef5beed04c0301b0fe48d4145f717d9fe0d
fiftysnd <llgaman008@gmail.com>
2024-11-25 15:08:40 +0700

    W11: Soal 7

--- lib/main.dart
+++ lib/main.dart
@@ -83,12 +83,11 @@ class _FuturePageState extends State<FuturePage> {
   }
 
   void returnFG() {
-    FutureGroup<int> futureGroup = FutureGroup<int>();
-    futureGroup.add(returnOneAsync());
-    futureGroup.add(returnTwoAsync());
-    futureGroup.add(returnThreeAsync());
-    futureGroup.close();
-    futureGroup.future.then((List<int> value) {
+    Future.wait<int>([
+      returnOneAsync(),
+      returnTwoAsync(),
+      returnThreeAsync(),
+    ]).then((List<int> value) {
       int total = 0;
       for (var element in value) {
         total += element;
